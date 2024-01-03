48ffacf180aae5e63289e7426fcf4810e35bae80
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-03 12:38:02 +0100

    submission Bite 3 @ codechalleng.es

--- 3/wordvalue.py
+++ 3/wordvalue.py
@@ -3,28 +3,41 @@ import urllib.request
 
 # PREWORK
 TMP = os.getenv("TMP", "/tmp")
-S3 = 'https://bites-data.s3.us-east-2.amazonaws.com/'
-DICT = 'dictionary.txt'
+S3 = "https://bites-data.s3.us-east-2.amazonaws.com/"
+DICT = "dictionary.txt"
 DICTIONARY = os.path.join(TMP, DICT)
-urllib.request.urlretrieve(f'{S3}{DICT}', DICTIONARY)
+urllib.request.urlretrieve(f"{S3}{DICT}", DICTIONARY)
 
-scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
-                   (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
-LETTER_SCORES = {letter: score for score, letters in scrabble_scores
-                 for letter in letters.split()}
+scrabble_scores = [
+    (1, "E A O I N R T L S U"),
+    (2, "D G"),
+    (3, "B C M P"),
+    (4, "F H V W Y"),
+    (5, "K"),
+    (8, "J X"),
+    (10, "Q Z"),
+]
+LETTER_SCORES = {
+    letter: score for score, letters in scrabble_scores for letter in letters.split()
+}
 
-# start coding
 
 def load_words():
     """Load the words dictionary (DICTIONARY constant) into a list and return it"""
-    pass
+    with open(DICTIONARY) as file:
+        words = file.read().splitlines()
+    return list(words)
 
 
 def calc_word_value(word):
     """Given a word calculate its value using the LETTER_SCORES dict"""
-    pass
+    word_value = 0
+    for char in word.upper():
+        if char.upper() in LETTER_SCORES:
+            word_value += LETTER_SCORES[char]
+    return word_value
 
 
 def max_word_value(words):
     """Given a list of words calculate the word with the maximum value and return it"""
-    pass
+    return max(words, key=calc_word_value)
