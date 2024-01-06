012b0bae13a6290c74577025a36dffc2e7ec697d
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-06 06:35:22 +0100

    submission Bite 7 @ codechalleng.es

--- 7/logtimes.py
+++ 7/logtimes.py
@@ -1,15 +1,14 @@
-from datetime import datetime
 import os
 import urllib.request
+from datetime import datetime
 
-SHUTDOWN_EVENT = 'Shutdown initiated'
+SHUTDOWN_EVENT = "Shutdown initiated"
 
 # prep: read in the logfile
 tmp = os.getenv("TMP", "/tmp")
-logfile = os.path.join(tmp, 'log')
+logfile = os.path.join(tmp, "log")
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/messages.log',
-    logfile
+    "https://bites-data.s3.us-east-2.amazonaws.com/messages.log", logfile
 )
 
 with open(logfile) as f:
@@ -18,21 +17,29 @@ with open(logfile) as f:
 
 # for you to code:
 
+
 def convert_to_datetime(line):
-    """TODO 1:
-       Extract timestamp from logline and convert it to a datetime object.
-       For example calling the function with:
-       INFO 2014-07-03T23:27:51 supybot Shutdown complete.
-       returns:
-       datetime(2014, 7, 3, 23, 27, 51)
     """
-    pass
+    Extract timestamp from logline and convert it to a datetime object.
+    For example calling the function with:
+    INFO 2014-07-03T23:27:51 supybot Shutdown complete.
+    returns:
+    datetime(2014, 7, 3, 23, 27, 51)
+    """
+    timestamp_str = line.split()[1]
+    return datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%S")
 
 
 def time_between_shutdowns(loglines):
-    """TODO 2:
-       Extract shutdown events ("Shutdown initiated") from loglines and
-       calculate the timedelta between the first and last one.
-       Return this datetime.timedelta object.
     """
-    pass
+    Extract shutdown events ("Shutdown initiated") from loglines and
+    calculate the timedelta between the first and last one.
+    Return this datetime.timedelta object.
+    """
+    shutdowns = []
+    for line in loglines:
+        if line.endswith("Shutdown initiated.\n"):
+            shutdowns.append(convert_to_datetime(line))
+
+    calculated_timedelta = shutdowns[1] - shutdowns[0]
+    return calculated_timedelta
