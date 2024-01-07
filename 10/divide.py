d1fccbbf6fd08f0adf76b4c29c7e2537f7081a34
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-07 06:55:48 +0100

    submission Bite 10 @ codechalleng.es

--- 10/divide.py
+++ 10/divide.py
@@ -1,2 +1,18 @@
+def is_valid_number(value, variable_name):
+    if not isinstance(value, (int, float)):
+        raise TypeError(f"{variable_name} moet een getal zijn")
+
+
 def positive_divide(numerator, denominator):
-    pass
+    is_valid_number(numerator, "Numerator")
+    is_valid_number(denominator, "Denominator")
+
+    if denominator == 0:
+        return 0
+
+    resultaat = numerator / denominator
+
+    if resultaat < 0:
+        raise ValueError("Het resultaat van de deling moet positief zijn")
+
+    return resultaat
