1ede83615d36e8b898e8c5700f6bbeb133e8d606
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-11 04:40:36 +0100

    submission Bite 14 @ codechalleng.es

--- 14/table.py
+++ 14/table.py
@@ -1,11 +1,18 @@
 import random
 
-names = ['Julian', 'Bob', 'PyBites', 'Dante', 'Martin', 'Rodolfo']
-aliases = ['Pythonista', 'Nerd', 'Coder'] * 2
+names = ["Julian", "Bob", "PyBites", "Dante", "Martin", "Rodolfo"]
+aliases = ["Pythonista", "Nerd", "Coder"] * 2
 points = random.sample(range(81, 101), 6)
 awake = [True, False] * 3
-SEPARATOR = ' | '
+SEPARATOR = " | "
 
 
-def generate_table():
-    pass
+def format_row(elements):
+    return SEPARATOR.join(str(element) for element in elements)
+
+
+def generate_table(*sequences):
+    results = []
+    for elements in zip(*sequences):
+        results.append(format_row(elements))
+    return results
