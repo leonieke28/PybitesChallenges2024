a42ac0edeb7db2c04a96265bf608eb5f3d59a141
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-20 06:03:30 +0100

    submission Bite 23 @ codechalleng.es

--- 23/similarity.py
+++ 23/similarity.py
@@ -5,22 +5,19 @@ import itertools
 from urllib.request import urlretrieve
 
 # prep
-TAG_HTML = re.compile(r'<category>([^<]+)</category>')
+TAG_HTML = re.compile(r"<category>([^<]+)</category>")
 TMP = os.getenv("TMP", "/tmp")
-TEMPFILE = os.path.join(TMP, 'feed')
+TEMPFILE = os.path.join(TMP, "feed")
 MIN_TAG_LEN = 10
 IDENTICAL = 1.0
 SIMILAR = 0.95
 
-urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/tags.xml',
-    TEMPFILE
-)
+urlretrieve("https://bites-data.s3.us-east-2.amazonaws.com/tags.xml", TEMPFILE)
 
 
 def _get_tags(tempfile=TEMPFILE):
     """Helper to parse all tags from a static copy of PyBites' feed,
-       providing this here so you can focus on difflib"""
+    providing this here so you can focus on difflib"""
     with open(tempfile) as f:
         content = f.read().lower()
     # take a small subset to keep it performant
@@ -32,4 +29,11 @@ def _get_tags(tempfile=TEMPFILE):
 def get_similarities(tags=None):
     """Should return a list of similar tag pairs (tuples)"""
     tags = tags or _get_tags()
-    # do your thing ...
+
+    similarities = []
+    for tag1, tag2 in itertools.combinations(tags, 2):
+        similarity_ratio = SequenceMatcher(None, tag1, tag2).ratio()
+        if similarity_ratio >= SIMILAR:
+            similarities.append((tag1, tag2))
+
+    return similarities
