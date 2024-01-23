46a72a9c22318d30dd1dc45d62ac8b8fe3de2be9
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-23 05:21:04 +0100

    submission Bite 28 @ codechalleng.es

--- 28/dates.py
+++ 28/dates.py
@@ -4,16 +4,16 @@ import os
 import re
 from urllib.request import urlretrieve
 
-BASE_URL = 'https://bites-data.s3.us-east-2.amazonaws.com/'
-RSS_FEED = 'pybites_feed.rss.xml'
-PUB_DATE = re.compile(r'<pubDate>(.*?)</pubDate>')
+BASE_URL = "https://bites-data.s3.us-east-2.amazonaws.com/"
+RSS_FEED = "pybites_feed.rss.xml"
+PUB_DATE = re.compile(r"<pubDate>(.*?)</pubDate>")
 TMP = os.getenv("TMP", "/tmp")
 
 
 def _get_dates():
     """Downloads PyBites feed and parses out all pub dates returning
-       a list of date strings, e.g.: ['Sun, 07 Jan 2018 12:00:00 +0100',
-       'Sun, 07 Jan 2018 11:00:00 +0100', ... ]"""
+    a list of date strings, e.g.: ['Sun, 07 Jan 2018 12:00:00 +0100',
+    'Sun, 07 Jan 2018 11:00:00 +0100', ... ]"""
     remote = os.path.join(BASE_URL, RSS_FEED)
     local = os.path.join(TMP, RSS_FEED)
     urlretrieve(remote, local)
@@ -24,10 +24,13 @@ def _get_dates():
 
 def convert_to_datetime(date_str):
     """Receives a date str and convert it into a datetime object"""
-    pass
+    return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %z")
 
 
 def get_month_most_posts(dates):
     """Receives a list of datetimes and returns the month (format YYYY-MM)
-       that occurs most"""
-    pass
+    that occurs most"""
+    dates = [date.strftime("%Y-%m") for date in dates]
+    month_counts = collections.Counter(dates)
+
+    return month_counts.most_common(1)[0][0]
