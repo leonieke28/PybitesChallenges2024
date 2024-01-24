a975e8a998c5e6e6dbbd8784bfaac61839806c99
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-24 05:56:53 +0100

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,15 @@
 def get_index_different_char(chars):
-    pass
+    unique_count = 1
+    alphanumeric_count = sum(1 for char in chars if str(char).isalnum())
+    non_alphanumeric_count = sum(1 for char in chars if not str(char).isalnum())
+    alphanumeric_index = next(
+        (i for i, char in enumerate(chars) if str(char).isalnum()), None
+    )
+    non_alphanumeric_index = next(
+        (i for i, char in enumerate(chars) if not str(char).isalnum()), None
+    )
+    return (
+        alphanumeric_index
+        if alphanumeric_count == unique_count
+        else non_alphanumeric_index
+    )
