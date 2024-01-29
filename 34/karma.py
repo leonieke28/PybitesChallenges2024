43a56c3fa7a4df8bff4820466c1a4edcba7175ca
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-01-29 06:06:59 +0100

    submission Bite 34 @ codechalleng.es

--- 34/karma.py
+++ 34/karma.py
@@ -2,10 +2,30 @@ from collections import namedtuple
 from datetime import datetime
 
 Transaction = namedtuple(
-    'Transaction',
-    'giver points date',
-    defaults=(None, None, datetime.now()))
+    "Transaction", "giver points date", defaults=(None, None, datetime.now())
+)
 
 
 class User:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._transactions = []
+
+    def __add__(self, transaction):
+        self._transactions.append(transaction)
+
+    @property
+    def points(self):
+        return [t.points for t in self._transactions]
+
+    @property
+    def karma(self):
+        return sum(t.points for t in self._transactions)
+
+    @property
+    def fans(self):
+        return len(set(t.giver for t in self._transactions))
+
+    def __str__(self):
+        fan_string = "fan" if self.fans == 1 else "fans"
+        return f"{self.name} has a karma of {self.karma} and {self.fans} {fan_string}"
