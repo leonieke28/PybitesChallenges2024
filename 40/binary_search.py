3510d392c3eeeb4b773e861175452635b0a72f9a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-02-04 05:47:18 +0100

    submission Bite 40 @ codechalleng.es

--- 40/binary_search.py
+++ 40/binary_search.py
@@ -1,2 +1,12 @@
 def binary_search(sequence, target):
-    pass
+    if not sequence:
+        return None
+    mid = len(sequence) // 2
+    if sequence[mid] == target:
+        return mid
+    elif sequence[mid] > target:
+        return binary_search(sequence[:mid], target)
+    else:
+        result = binary_search(sequence[mid + 1 :], target)
+        if result is not None:
+            return mid + 1 + result
