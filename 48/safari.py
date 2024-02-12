96c3f40a421cdfa9e8368d1b7d539223f668828a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-02-12 06:13:20 +0100

    submission Bite 48 @ codechalleng.es

--- 48/safari.py
+++ 48/safari.py
@@ -1,16 +1,36 @@
 import os
+import re
 import urllib.request
 
 TMP = os.getenv("TMP", "/tmp")
-DATA = 'safari.logs'
+DATA = "safari.logs"
 SAFARI_LOGS = os.path.join(TMP, DATA)
-PY_BOOK, OTHER_BOOK = '🐍', '.'
+PY_BOOK, OTHER_BOOK = "🐍", "."
 
 urllib.request.urlretrieve(
-    f'https://bites-data.s3.us-east-2.amazonaws.com/{DATA}',
-    SAFARI_LOGS
+    f"https://bites-data.s3.us-east-2.amazonaws.com/{DATA}", SAFARI_LOGS
 )
 
 
 def create_chart():
-    pass
+    with open(SAFARI_LOGS) as f:
+        logs = f.readlines()
+
+    books_by_date = {
+        date: ""
+        for date in set(re.search(r"\d{2}-\d{2}", line).group() for line in logs)
+    }
+
+    for i, line in enumerate(logs):
+        if "sending to slack channel" in line:
+            date = re.search(r"\d{2}-\d{2}", line).group()
+            if "Python" in logs[i - 1]:
+                books_by_date[date] += PY_BOOK
+            else:
+                books_by_date[date] += OTHER_BOOK
+
+    for date in sorted(books_by_date.keys()):
+        print(date, books_by_date[date])
+
+
+create_chart()
