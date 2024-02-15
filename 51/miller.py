bdd389bca7c93192e33d4f64a09f0a6db8f331d0
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-02-15 09:48:46 +0100

    submission Bite 51 @ codechalleng.es

--- 51/miller.py
+++ 51/miller.py
@@ -2,16 +2,18 @@ from datetime import datetime
 
 # https://pythonclock.org/
 PY2_DEATH_DT = datetime(year=2020, month=1, day=1)
-BITE_CREATED_DT = datetime.strptime('2018-02-26 23:24:04', '%Y-%m-%d %H:%M:%S')
+BITE_CREATED_DT = datetime.strptime("2018-02-26 23:24:04", "%Y-%m-%d %H:%M:%S")
 
 
 def py2_earth_hours_left(start_date=BITE_CREATED_DT):
     """Return how many hours, rounded to 2 decimals, Python 2 has
-       left on Planet Earth (calculated from start_date)"""
-    pass
+    left on Planet Earth (calculated from start_date)"""
+    time_left = PY2_DEATH_DT - start_date
+    return round(time_left.total_seconds() / 3600, 2)
 
 
 def py2_miller_min_left(start_date=BITE_CREATED_DT):
     """Return how many minutes, rounded to 2 decimals, Python 2 has
-       left on Planet Miller (calculated from start_date)"""
-    pass
+    left on Planet Miller (calculated from start_date)"""
+    time_left = PY2_DEATH_DT - start_date
+    return round(time_left.total_seconds() / ((3600 * 24 * 365 * 7) / 60), 2)
