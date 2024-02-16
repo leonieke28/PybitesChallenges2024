d8565aca6e41cb3acb4ce9b816b75cba47519ca9
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-02-16 10:23:52 +0100

    submission Bite 52 @ codechalleng.es

--- 52/quotes.py
+++ 52/quotes.py
@@ -23,32 +23,63 @@ quotes = [
 
 def _get_quote(qid):
     """Recommended helper"""
+    for quote in quotes:
+        if quote.get("id") == qid:
+            return quote
+    else:
+        abort(404)
 
 
 def _quote_exists(existing_quote):
     """Recommended helper"""
+    return any(
+        quote["quote"] == existing_quote["quote"]
+        and quote["movie"] == existing_quote["movie"]
+        for quote in quotes
+    )
 
 
-@app.route('/api/quotes', methods=['GET'])
+@app.route("/api/quotes", methods=["GET"])
 def get_quotes():
-    pass
+    return jsonify({"quotes": quotes}), 200
 
 
-@app.route('/api/quotes/<int:qid>', methods=['GET'])
+@app.route("/api/quotes/<int:qid>", methods=["GET"])
 def get_quote(qid):
-    pass
+    return jsonify({"quotes": [_get_quote(qid)]})
 
 
-@app.route('/api/quotes', methods=['POST'])
+@app.route("/api/quotes", methods=["POST"])
 def create_quote():
-    pass
+    if not request.json or not "quote" in request.json or not "movie" in request.json:
+        abort(400)
+    if _quote_exists(request.json):
+        abort(400)
+    quote = {
+        "id": quotes[-1]["id"] + 1,
+        "quote": request.json["quote"],
+        "movie": request.json["movie"],
+    }
+    quotes.append(quote)
+    return jsonify({"quote": quote}), 201
 
 
-@app.route('/api/quotes/<int:qid>', methods=['PUT'])
+@app.route("/api/quotes/<int:qid>", methods=["PUT"])
 def update_quote(qid):
-    pass
+    quote = _get_quote(qid)
+    if quote is None:
+        abort(404)
+    if not request.json or not "quote" in request.json or not "movie" in request.json:
+        abort(400)
+    quote["quote"] = request.json.get("quote", quote["quote"])
+    quote["movie"] = request.json.get("movie", quote["movie"])
+    return jsonify({"quote": quote}), 200
 
 
-@app.route('/api/quotes/<int:qid>', methods=['DELETE'])
+@app.route("/api/quotes/<int:qid>", methods=["DELETE"])
 def delete_quote(qid):
-    pass
+    quote = _get_quote(qid)
+    if quote is None:
+        abort(404)
+    quotes.remove(quote)
+    return jsonify({}), 204
