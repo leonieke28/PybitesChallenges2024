e7a6b8f0ebf5a6e7288f5b2a85e114deaafae6a9
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-03-01 10:24:42 +0100

    submission Bite 54 @ codechalleng.es

--- 54/poem.py
+++ 54/poem.py
@@ -2,4 +2,15 @@ INDENTS = 4
 
 
 def print_hanging_indents(poem):
-    pass
+    lines = poem.splitlines()
+
+    first_line = True
+
+    for line in lines:
+        if line == "":
+            first_line = True
+        elif first_line:
+            print(line.strip())
+            first_line = False
+        else:
+            print(INDENTS * " " + line.strip())
