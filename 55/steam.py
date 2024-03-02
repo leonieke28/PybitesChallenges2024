b6cfc4b604311c07a59220355800f7f8f8822383
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-03-02 11:53:54 +0100

    submission Bite 55 @ codechalleng.es

--- 55/steam.py
+++ 55/steam.py
@@ -5,9 +5,17 @@ import feedparser
 # cached version to have predictable results for testing
 FEED_URL = "https://bites-data.s3.us-east-2.amazonaws.com/steam_gaming.xml"
 
-Game = namedtuple('Game', 'title link')
+Game = namedtuple("Game", "title link")
 
 
 def get_games():
     """Parses Steam's RSS feed and returns a list of Game namedtuples"""
-    pass
+    feed = feedparser.parse(FEED_URL)
+
+    games = []
+    for game in feed["items"]:
+        title = game["title"]
+        link = game["link"]
+        games.append(Game(title, link))
+
+    return games
