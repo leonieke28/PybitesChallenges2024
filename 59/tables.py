08630f3951be842f7330dfb6c5154d6eecea653e
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-03-17 07:57:29 +0100

    submission Bite 59 @ codechalleng.es

--- 59/tables.py
+++ 59/tables.py
@@ -1,18 +1,21 @@
 class MultiplicationTable:
-
     def __init__(self, length):
         """Create a 2D self._table of (x, y) coordinates and
-           their calculations (form of caching)"""
-        pass
+        their calculations (form of caching)"""
+        self._table = [
+            [x * y for x in range(1, length + 1)] for y in range(1, length + 1)
+        ]
 
     def __len__(self):
         """Returns the area of the table (len x* len y)"""
-        pass
+        return len(self._table) ** 2
 
     def __str__(self):
         """Returns a string representation of the table"""
-        pass
+        return "\n".join([" | ".join(map(str, row)) for row in self._table])
 
     def calc_cell(self, x, y):
         """Takes x and y coords and returns the re-calculated result"""
-        pass
+        if x < 1 or x > len(self._table) or y < 1 or y > len(self._table):
+            raise IndexError("x and y coordinates must be within the table boundaries")
+        return self._table[x - 1][y - 1]
