a356906c26a26bf2d759c3c5d2472d25f507a4fa
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-03-18 07:24:30 +0100

    submission Bite 61 @ codechalleng.es

--- 61/paw.py
+++ 61/paw.py
@@ -1,11 +1,18 @@
 from collections import namedtuple
 import random
 
-ACTIONS = ['draw_card', 'play_again',
-           'interchange_cards', 'change_turn_direction']
+ACTIONS = ["draw_card", "play_again", "interchange_cards", "change_turn_direction"]
 NUMBERS = range(1, 5)
 
-PawCard = namedtuple('PawCard', 'card action')
+PawCard = namedtuple("PawCard", "card action")
+
 
 def create_paw_deck(n=8):
-    pass
+    cards = [f"{chr(i + 65)}{j + 1}" for i in range(n) for j in range(4)]
+    actions = [None] * (n * 3) + ACTIONS * (n // 4)
+    random.shuffle(actions)
+    deck = [PawCard(card, action) for card, action in zip(cards, actions)]
+    if n > 26:
+        raise ValueError
+
+    return deck
